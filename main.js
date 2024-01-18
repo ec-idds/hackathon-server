4060cc769cf6f8ece2500c2b383bf6340aa09fd6
James Fink <finkj@emmanuel.edu>
2024-01-18 14:00:10 -0500

    add pong server file

--- main.js
+++ main.js
@@ -1,43 +1,82 @@
+const express = require("express");
+const { fstat } = require("fs");
+const fs = require("fs")
 const http = require("http");
 const path = require("path");
-const fs = require("fs");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
+
+let posY = 0;
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        fs.readFile(
-            path.join(__dirname, "public", "index.html"), 
-            (err, content) => {
-                if(err) throw err;
-                res.writeHead(200, {"Content-Type": 'text/html'});
-                res.end(content);
-            }
-        );
+    let filePath = path.join(__dirname, req.url === "/" ? "index.html"
+    : req.url);
+
+    //Extension of file
+    let extname = path.extname(filePath);
+
+    //Initial content type
+    let contentType = `text/html`;
+
+    //Check ext and set content type
+    switch(extname) {
+        case ".js":
+            contentType = "text/javascript"
+            break;
+        case ".css":
+            contentType = "text/css"
+            break;
+        case ".json":
+            contentType = "application/json"
+            break;
+        case ".png":
+            contentType = "image/png";
+            break;
+        case ".jpg":
+            contentType = "image/jpg";
+            break;
     }
 
-    if (req.url === "/about") {
-        fs.readFile(
-            path.join(__dirname, "public", "about.html"), 
-            (err, content) => {
-                if(err) throw err;
-                res.writeHead(200, {"Content-Type": 'text/html'});
-                res.end(content);
+    //Read File
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if(err.code == "ENOENT") {
+                // Page not found
+                console.log("Page not found! Oops!");
+            } else {
+                //Some Servor Error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
             }
-        );
-    }
-
-    if (req.url === "/api/users") {
-        const users = [
-            {name: "Jason", age: 22},
-            {name: "Erica", age: 20}
-        ];
-        res.writeHead(200, {"Content-Type": 'application/json'});
-        res.end(JSON.stringify(users));
-    }
+        } else {
+            //Success
+            res.writeHead(200, {"content-Type": contentType})
+            res.end(content, "utf8");
+        }
+    });
 });
 
-const PORT = process.env.PORT || 5000
+const io = new Server(server);
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+io.on("connection", (socket) => {
+    socket.on("requests", (arg) => {
+        posY = arg;
+        console.log(posY)
+    })
+    socket.on("update", (arg) => {
+        socket.emit("responses", posY);
+    })
+    //socket.on("chat message", (msg) => {
+        //io.emit("chat message", msg);
+    //});
+});
 
+const PORT = process.env.PORT || 3000;
+
+//server.maxConnections = 2;
+
+server.listen(PORT, () => {
+    console.log("server running at http://localhost:3000");
+});
 
 
--- sketch.js
+++ sketch.js
@@ -1,17 +1,55 @@
-const p5 = require("p5")
-const p5Play = require("p5play")
+let paddle;
+let paddle2;
+let ball;
+let rec = false;
 
-function setup(p5) {
-    p5.createCanvas(400);
+function setup() {
+    new Canvas(400, 400)
+    paddle = new Sprite();
+    paddle.width = 10;
+    paddle.height = 75;
+    paddle.x = 15;
+    paddle.color = "white";
+
+    paddle2 = new Sprite();
+    paddle2.width = 10;
+    paddle2.height = 75;
+    paddle2.x = width - 10;
+    paddle2.color = "white";
+
+    ball = new Sprite();
+    ball.width = 20;
+    ball.height = 20;
+    ball.color = "white";
+}
+
+function movement() {
+    if(kb.presses("up") && !rec) {
+        socket.emit("requests", -1);
+        rec = true;
+    } else if (kb.presses("down") && !rec) {
+        socket.emit("requests", 1);
+        rec = true;
+    }
+}
+
+function update() {
+    socket.emit("update", true);
+    socket.on("responses", (arg) => {
+        paddle.y += arg;
+        console.log("updated", paddle.y);
+        rec = false;
+    })
+}
+
+function draw() {
+    background("black");
+    movement();
+    if (frameCount % 2 === 0) {
+        update();
+    }
 }
 
 function preload() {
 
 }
-
-function draw() {
-    background(255);
-    rect(20, 20, 20);
-}
-
-// test comment
